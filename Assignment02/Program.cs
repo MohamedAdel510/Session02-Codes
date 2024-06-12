4f8419011796ad979c615818b7567c14b4afa08a
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-06-13 00:01:35 +0300

    Add project files.

---
+++ Assignment02/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.WriteLine("Enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You entered: " + num);
+            #endregion
+
+            #region Q2
+            //string x = "Ahmed";
+            //Console.WriteLine(Convert.ToInt32(x));
+            //runtime error (unlogical Exception)
+            #endregion
+
+            #region Q3
+            //float x = Convert.ToSingle(Console.ReadLine());
+            //float y = Convert.ToSingle(Console.ReadLine());
+            //Console.WriteLine(x + y);
+            #endregion
+
+            #region Q4
+            //string s = "Ahmed";
+            //Console.WriteLine(s.Substring(0, s.Length - 3));
+            #endregion
+
+            #region Q5
+            //int x; 
+            //x = 4;
+
+            //int y = 5;
+            //y = x;
+
+            //x++;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //// any modification on x or y will not affect on the other variable
+            #endregion
+
+            #region Q6
+            //Point p1 = new Point(); 
+
+            //Point p2 = new Point();
+
+            //Console.WriteLine(p2.X);
+
+            //p2 = p1;
+            //p1.X = 4;
+
+            //Console.WriteLine(p2.X);
+
+            ////p2 = p1 means p2 now point to p1 so any modification on p1 will affect on p2
+            #endregion
+
+            #region Q7
+            //Console.WriteLine("Enter First name: ");
+            //string Fname = Console.ReadLine();
+            //Console.WriteLine("Enter Last name: ");
+            //string Lname = Console.ReadLine();
+            //Console.WriteLine(Fname + ' ' + Lname);
+            //// or 
+            //Console.WriteLine($"{Fname} {Lname}");
+            #endregion
+
+            #region Q8
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);
+            // so  value 1 will be assigned to d.
+            #endregion
+
+            #region Q9
+            //  the output will be: 6 1
+            #endregion
+
+            #region Q10
+            // output will be: 7 7
+            #endregion
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,133 @@
+﻿namespace Demo
+{
+    //Class
+    //Struct
+    //Interface
+    //Enum
+    internal class Program
+    {
+        //Functions
+        void test01()
+        {
+
+        }
+        void test03()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            #region first prgram
+            // Console.WriteLine("Hello, World!");  single line comment 
+            /*           
+            Console.ReadLine();                     multi line  comment      
+
+            */
+            #endregion
+
+            #region Namming Convention
+            // camel case ===> nourhanTariq
+            // pascal Case ===> NourhanTariq
+            // Kebab case ===> Nourhan_Tariq
+            #endregion
+
+            #region value type
+            //int x; // Allocate unintialized 4 byte in stack
+            //x = 4;
+
+            //Int32 y = 5;
+
+            //y = x;
+            //x++;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            #endregion
+
+            #region Reference type
+            //Point p1; // Allocate 4 byte in stack
+            //          // Zero byte in heap
+            //p1 = new Point(); // Allocate required byte for object of point at heap
+            //                  // intialized allocated bytes of object
+            //                  // call user defind constructor if exist
+            //                  // Assigned reference (p1) to the allocated opject
+            //Point p2 = new Point();
+
+            //p2 = p1;
+            //p1.x = 9;
+
+            //Console.WriteLine(p2.x);
+            #endregion
+
+            #region object
+            //Point p1 = new Point();
+            //Point p2 = new Point();
+
+            //Console.WriteLine(p1.ToString());
+
+            //Console.WriteLine(p1.GetHashCode());
+
+            //Console.WriteLine(p2.Equals(p1));
+
+            //Console.WriteLine(p1.GetType());
+
+            //p1 = p2;
+
+            //Console.WriteLine(p2.Equals(p1));
+
+            //object x;
+            //x = new string("Ahmed");
+            //x = "Ahmed";             // syntax sugar
+            //x = 'A';
+            //x = 3;
+            //x = 3.7;
+            //x = new DateTime();
+            //x = new DateOnly();
+            //x = new TimeOnly();
+
+            //object x1 = new Object();
+            //object x2 = new Object();
+
+            //Console.WriteLine(x1.GetHashCode());
+            //Console.WriteLine(x2.GetHashCode());
+
+            //x1 = x2;
+            //Console.WriteLine("\nAfter Equal\n");
+
+            //Console.WriteLine(x1.GetHashCode());
+            //Console.WriteLine(x2.GetHashCode());
+            #endregion
+
+            #region casting
+            // Parse Method
+            //Console.WriteLine("Enter your name\n");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Hello, {name}!\n");
+            //Console.WriteLine("Enter your age\n");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine("Your name: " + name +"\n");
+            //Console.WriteLine("Your age: " + age + "\n");
+
+            // Convert class
+
+            //Console.WriteLine("Enter your name\n");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Hello, {name}!\n");
+            //Console.WriteLine("Enter your age\n");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine("Your name: " + name + "\n");
+            //Console.WriteLine("Your age: " + age + "\n");
+
+            // Convert disadvatdes
+
+            //string x = "Nourhan";
+            //bool flag = Convert.ToBoolean(x);
+            //Console.WriteLine(flag);        // Exception
+
+            #endregion
+
+        }
+    }
+}
